dbf71f8b15f7bb3eec2b91f01788f638b5125a2f
R. S. Doiel <rsdoiel@gmail>
2025-01-16 14:49:26 -0800

    forked colly so I can fix dependbot issues

--- version.go
+++ version.go
@@ -12,7 +12,7 @@ const (
     ReleaseDate = "2025-01-16"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "ed23b9c"
+    ReleaseHash = "aa80afc"
 
     LicenseText = `
 Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
